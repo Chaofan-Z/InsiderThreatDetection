e93945551c190fc982cde444648cdeb5b9130e0b
ChaofanZheng <cfzheng@pku.edu.cn>
2021-12-01 15:41:23 +0800

    Start to construct graph

---
+++ construct_rule.py
@@ -0,0 +1,17 @@
+import numpy as np
+import networkx as nx
+from tqdm import tqdm
+
+# 一个用户同天同一个host时序连接
+def rule_1(activity_graph, daily_sequences_list, day_delta):
+    pass
+
+# 一个用户多天同一个host的行为链的时序关联
+def rule_2(activity_graph, daily_sequences_list, day_delta):
+    pass
+
+# 一个用户多天同一个host同种组操作类型时序关联
+# （规则定义组操作类型，比如Connect-> disconnect, File open -> File Write, visit web...）
+def rule_3(activity_graph, daily_sequences_list, day_delta):
+    pass
+
